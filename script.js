a992c7f88dcc91e7f1f02461ed4fd9bf3e76d6bc
ITZBaburam <139737597+Baburam99@users.noreply.github.com>
2024-05-06 01:33:49 +0545

    error fix

--- script.js
+++ script.js
@@ -1,30 +1,49 @@
 let closeNav = document.getElementById('closeNav');
 let openbtn = document.getElementById('open');
 let closebtn = document.getElementById('close');
+let body = document.getElementById('body');
 let main = document.getElementById('main');
-let overlay = document.getElementById('overlay');
+let project = document.getElementById('project');
 
 openbtn.addEventListener("click", function(){
-  openbtn.style.display = "none";
+  openbtn.style.display = "block";
   closebtn.style.display = "block";
   closeNav.style.left = "30vw"; 
-  overlay.style.display = "block"; // Show the dark overlay
+  overlay.style.display = "block";
 });
 
 closebtn.addEventListener("click", function(){
   closebtn.style.display = "none";
   openbtn.style.display = "block";
   closeNav.style.left = "100vw";
-  overlay.style.display = "none"; // Hide the dark overlay
+  overlay.style.display = "none";
 });
 
+
 // Add event listeners to each navigation link
-let navLinks = document.querySelectorAll('.nav-link');
+let navLinks = document.querySelectorAll('#nav li a');
 navLinks.forEach(function(link) {
   link.addEventListener('click', function() {
     closebtn.style.display = "none";
     openbtn.style.display = "block";
-    closeNav.style.left = "-30vw";
-    overlay.style.display = "none"; // Hide the dark overlay
+    closeNav.style.left = "100vw";
+    overlay.style.display = "none";
   });
+});
+
+// Add event listener to each navigation link in the hidden menu
+let hiddenNavLinks = document.querySelectorAll('#closeNav li a');
+hiddenNavLinks.forEach(function(link) {
+  link.addEventListener('click', function() {
+    closebtn.style.display = "none";
+    openbtn.style.display = "block";
+    closeNav.style.left = "100vw";
+    overlay.style.display = "none";
+  });
+});
+overlay.addEventListener("click", function(){
+  closebtn.style.display = "none";
+  openbtn.style.display = "block";
+  closeNav.style.left = "100vw";
+  overlay.style.display = "none";
 });
