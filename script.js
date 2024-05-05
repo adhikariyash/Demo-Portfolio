f87cec1458e005f810039dd576b59d8617ab7631
ITZBaburam <139737597+Baburam99@users.noreply.github.com>
2024-05-05 23:10:06 +0545

    just some thins

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let closeNav = document.getElementById('closeNav');
 let openbtn = document.getElementById('open');
 let closebtn = document.getElementById('close');
 let body = document.getElementById('body');
+let main = document.getElementById('main')
 
 
 openbtn.addEventListener("click", function(){
@@ -9,6 +10,7 @@ openbtn.addEventListener("click", function(){
   closebtn.style.display = "block";
   closeNav.style.left = "30vw"; 
   body.style.position ="fixed"
+  main.style.opacity = "0%"
   // Adjust the left position as needed
 });
 
@@ -16,5 +18,8 @@ closebtn.addEventListener("click", function(){
   closebtn.style.display = "none";
   openbtn.style.display = "block";
   closeNav.style.left = "100vw";
-  body.style.position = "relative" // Adjust the left position to hide the navigation menu
+  body.style.position = "relative"
+  main.style.opacity = "100%"
+
+   // Adjust the left position to hide the navigation menu
 });
