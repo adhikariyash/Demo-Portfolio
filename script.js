85570665e03254d0cf1d425382ddfe925884ad07
ITZBaburam <139737597+Baburam99@users.noreply.github.com>
2024-05-05 22:55:39 +0545

    buttons

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 let closeNav = document.getElementById('closeNav');
 let openbtn = document.getElementById('open');
 let closebtn = document.getElementById('close');
+let body = document.getElementById('body');
+
 
 openbtn.addEventListener("click", function(){
   openbtn.style.display = "none";
   closebtn.style.display = "block";
-  closeNav.style.left = "30vw"; // Adjust the left position as needed
+  closeNav.style.left = "30vw"; 
+  body.style.position ="fixed"
+  // Adjust the left position as needed
 });
 
 closebtn.addEventListener("click", function(){
   closebtn.style.display = "none";
   openbtn.style.display = "block";
-  closeNav.style.left = "100vw"; // Adjust the left position to hide the navigation menu
+  closeNav.style.left = "100vw";
+  body.style.position = "relative" // Adjust the left position to hide the navigation menu
 });
